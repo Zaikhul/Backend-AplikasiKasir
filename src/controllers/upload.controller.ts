1e75b382c337234d8739adead97646d450a59bc0
Zaikhul Sulthon Suasono <zaikhulsulthon@gmail.com>
2025-11-26 13:58:40 +0700

    Fixing:Upload Image from phone

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -42,7 +42,7 @@ export class UploadController {
         },
       }),
       limits: {
-        fileSize: 5 * 1024 * 1024, // 5MB
+        fileSize: 10 * 1024 * 1024, // 10MB
       },
       fileFilter: (req, file, cb) => {
         const allowedMimes = [
@@ -50,6 +50,8 @@ export class UploadController {
           'image/png',
           'image/jpg',
           'image/webp',
+          'image/heic',
+          'image/heif',
         ];
         if (allowedMimes.includes(file.mimetype)) {
           cb(null, true);
